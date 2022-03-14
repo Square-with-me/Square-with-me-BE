6e0c1c79f15e0472800ae6b4a0999fb5cf50648b
seongwon <87584033+Magiof@users.noreply.github.com>
2022-03-14 17:47:47 +0900

    FIX: 영상채팅 중간 완료

--- socket.js
+++ socket.js
@@ -9,237 +9,93 @@ const io = require("socket.io")(server, {
   },
 });
 
-// models
-// const room = require("./models/room");
-const user = require("./models/user");
-
-//방에 몇명 있는지
-function roomSize(roomID) {
-  return io.sockets.adapter.rooms.get(roomID)?.size;
-}
+//controller
+const RoomController = require("./controllers/roomController");
 
 const users = {};
 const socketToRoom = {};
 const socketToNickname = {};
 
 io.on("connection", (socket) => {
-  // 참가자 관전자 나눈거
-  // 방에 들어오면
-  // socket.on("join room", async (roomID, userID, role) => {
-  //   if (!roomID || !userID || !role) {
-  //     return;
-  //   }
-  //   if (!users[roomID]) {
-  //     users[roomID] = {};
-  //     users[roomID].participants = [];
-  //     users[roomID].viewers = [];
-  //   }
-  //   //roomID, role
-  //   //asyncWrapper 쓰기
-  //   try {
-  //     let mySocketId = socket.id;
+  socket.on("join room", async (payload, done) => {
+    const roomId = payload.roomId;
+    const nickname = payload.nickname;
 
-  //     //해당 방 안에 있는 사람들에게 영상공유를 할 수 있는 재료들을 건네줌
-  //     if (role === "participants") {
-  //       //참가자로 들어오면
-  //       if (users[roomID].participants) {
-  //         //이미 참가자가 있을때 배열에 push
-  //         if (users[roomID].participants.length >= 4) {
-  //           return alert("참가자 풀방");
-  //         }
-  //         users[roomID].participants.push(socket.id);
-  //       } else {
-  //         //참가자 배열이 존재하지 않으면 배열만들어주기
-  //         users[roomID].participants = [socket.id];
-  //       }
-  //     } else if (role === "viewers") {
-  //       //관전자로 들어오면
-  //       if (users[roomID].viewers) {
-  //         //이미 관전자가 있을때 배열에 push
-  //         if (users[roomID].viewers.length >= 5) {
-  //           return alert("관전자 풀방");
-  //         }
-  //         users[roomID].viewers.push(socket.id);
-  //       } else {
-  //         //관전자 배열이 존재하지 않으면 배열만들어주기
-  //         users[roomID].viewers = [socket.id];
-  //       }
-  //     } else {
-  //       return console.log("잘못된 접근");
-  //     }
-  //     socket.join(roomID);
-  //     console.log(io.sockets.adapter.rooms);
-  //     socketToRoom[socket.id] = roomID; //부딪히며 이해하기
-  //     const participantsRoom = users[roomID].participants.filter(
-  //       (id) => id !== socket.id
-  //     );
-  //     const viewersInthisRoom = users[roomID].viewers.filter(
-  //       (id) => id !== socket.id
-  //     );
-  //     console.log(participantsRoom);
-  //     // console.log(users);
-  //     //usersInThisRoom을 참가자와 관전자로 나눠서 관전자의 peer는 video와 audio를 false로 할 수 있게 만듬
-  //     //roomSize함수로 실시간 현재 총 인원을 방안에 표기함 (참가자,관전자 구분 안함)
-  //     // socket.to(data.roomID)  //roomID안에 모든사람(나 자신 제외)
-  //     // io.sockets.in(data.roomID)  //roomID안에 모든사람 (나 자신 포함)
-  //     socket.emit("room users", participantsRoom, viewersInthisRoom);
-  //     socket.to(roomID).emit("newbie", mySocketId);
-  //   } catch (error) {
-  //     console.log(error);
-  //   }
-  // });
+    if (!roomId || !nickname) {
+      socket.emit("no data");
+    };
 
-  //참가자 관전자 안나눈거
-  socket.on("join room", async (roomID, nickname, done) => {
-    if (!roomID || !nickname) {
-      return console.log("뭔가 없다");
-    }
-    if (users[roomID]) {
-      if (users[roomID].length === 4) {
-        done();
-        return console.log("참가자 풀방");
-      }
-      users[roomID].push(socket.id);
-    } else {
-      //배열이 존재하지 않으면 배열만들어주기
-      users[roomID] = [socket.id];
-    }
-    console.log("현재", roomID, "방안에 있는 인원 수는", users[roomID].length);
-    socket.join(roomID);
-    console.log(io.sockets.adapter.rooms);
-    socketToRoom[socket.id] = roomID; //각각의 소켓아이디가 어떤 룸에 들어가는지
+    if(users[roomId]) {  // 기존 참가자 있음
+      if(users[roomId].length >= 4) { // 참가자 풀방
+        return done();
+      };
+
+      users[roomId].push(socket.id);
+    } else {  // 첫 참가자
+      users[roomId] = [socket.id];
+    };
+
+    socket.join(roomId);
+
+    socketToRoom[socket.id] = roomId; //각각의 소켓아이디가 어떤 룸에 들어가는지
     socketToNickname[socket.id] = nickname;
-    const otherInRoom = users[roomID].filter((id) => id !== socket.id);
-    console.log(otherInRoom);
-    // console.log(users);
-    //usersInThisRoom을 참가자와 관전자로 나눠서 관전자의 peer는 video와 audio를 false로 할 수 있게 만듬
-    //roomSize함수로 실시간 현재 총 인원을 방안에 표기함 (참가자,관전자 구분 안함)
-    // socket.to(data.roomID)  //roomID안에 모든사람(나 자신 제외)
-    // io.sockets.in(data.roomID)  //roomID안에 모든사람 (나 자신 포함)
-    socket.emit("room users", otherInRoom);
+
+    let otherUsers = users[roomId].filter((socketId) => socketId !== socket.id);
+
+    otherUsers = otherUsers.map((socketId)=> {
+      return {
+        socketId: socketId,
+        nickname: socketToNickname[socket.id]
+      };
+    });
+
+    socket.emit("send users", otherUsers);
   });
 
-  //빡빡이 따라함
-  socket.on("sending signal", (payload) => {
-    const nickname = socketToNickname[payload.callerID];
-    io.to(payload.userToSignal).emit(
-      "user joined",
-      {
+  socket.on("send signal", (payload) => {
+    const callerNickname = socketToNickname[payload.callerId];
+
+    io.to(payload.userToSignal).emit("user joined", {
         signal: payload.signal,
-        callerID: payload.callerID,
+        callerId: payload.callerId,
+        callerNickname,
       },
-      nickname
-    );
-    console.log(
-      "센딩시그널콘솔 요청하는사람 ",
-      payload.callerID,
-      "받는사람 ",
-      payload.userToSignal
     );
   });
 
-  //빡빡이 따라함
   socket.on("returning signal", (payload) => {
-    io.to(payload.callerID).emit("receiving returned signal", {
+    io.to(payload.callerId).emit("receive returned signal", {
       signal: payload.signal,
       id: socket.id,
     });
-    console.log("리터닝시그널");
   });
 
-  //disconnecting event는 socket이 방을 떠나기 바로 직전 발생합니다!
-  // socket.on("disconnecting", () => {
-
-  //   socket.rooms.forEach((roomID) =>
-  //     socket.to(socket.id).emit("bye", socket.id, roomSize(roomID) - 1)
-  //   );
-  //   console.log("disconnecting거쳐감");
-  //   //bye에서는 작별인사하기
-  // });
+  socket.on("end", async (payload, done) => {
+    const data = {
+      userId: payload.userId,
+      time: payload.time,
+      categoryId: payload.categoryId,
+      date: payload.date,
+    }
+    
+    RoomController.delete.participant(data);
+    done();
+  });
 
   socket.on("disconnecting", () => {
-    const roomID = socketToRoom[socket.id];
-    if (users[roomID]) {
-      users[roomID] = users[roomID].filter((id) => id !== socket.id);
-    }
-    console.log("left", users);
-    socket.broadcast.emit("user left", socket.id, socketToNickname[socket.id]);
-    console.log(socketToNickname);
+    const roomId = socketToRoom[socket.id];
+
+    if(users[roomId]) {
+      users[roomId] = users[roomId].filter((id) => id !== socket.id);
+    };
+
+    socket.broadcast.emit("user left", {
+      socketId: socket.id,
+      nickname: socketToNickname[socket.id]
+    });
+
     delete socketToNickname[socket.id];
-    console.log(socketToNickname);
   });
-
-  ///////아마 채팅기능///////
-  //   socket.on("message", (message) => {
-  //     socket.to(roomID).emit("message", nickname, message);
-  //   });
-
-  //   socket.on("join-chatRoom", (roomId, userId, userNickname) => {
-  //     socket.join(roomId);
-  //   });
 });
 
-//////////////////빡빡쓰///////////////
-// const users = {};
-
-// const socketToRoom = {};
-
-// io.on('connection', socket => {
-//   socket.on("join room", roomID => {
-//       if (users[roomID]) {
-//           const length = users[roomID].length;
-//           if (length === 4) {
-//               socket.emit("room full");
-//               return;
-//           }
-//           users[roomID].push(socket.id);
-//       } else {
-//           users[roomID] = [socket.id];
-//       }
-//       socketToRoom[socket.id] = roomID;
-//       console.log(socketToRoom);
-//       const usersInThisRoom = users[roomID].filter(id => id !== socket.id);
-
-//       socket.emit("all users", usersInThisRoom);
-//   });
-
-//   socket.on("sending signal", payload => {
-//       io.to(payload.userToSignal).emit('user joined', { signal: payload.signal, callerID: payload.callerID });
-//   });
-
-//   socket.on("returning signal", payload => {
-//       io.to(payload.callerID).emit('receiving returned signal', { signal: payload.signal, id: socket.id });
-//   });
-
-//   socket.on('disconnect', () => {
-//       const roomID = socketToRoom[socket.id];
-//       let room = users[roomID];
-//       if (room) {
-//           room = room.filter(id => id !== socket.id);
-//           users[roomID] = room;
-//       }
-//   });
-
-// });
-
 module.exports = { server };
-
-// 카메라 바꾸는거 (프론트)
-// async function getCameras() {
-//   try {
-//       const devies = await navigator.mediaDevices.enumerateDevices();
-//       const cameras = devies.filter((device) => device.kind === 'videoinput');
-//       const currentCamera = myStream.getVideoTracks()[0];
-//       cameras.forEach((camera) => {
-//           const option = document.createElement('option');
-//           option.value = camera.deviceId;
-//           option.innerText = camera.label;
-//           if (currentCamera.label === camera.label) {
-//               option.selected = true;
-//           }
-//           camerasSelect.appendChild(option);
-//       });
-//   } catch (e) {
-//       console.log(e);
-//   }
-// }
