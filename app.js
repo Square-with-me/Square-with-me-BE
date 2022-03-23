0e5f217bb1d1c5a68b7135c3f7c32def6e89ac92
Hyeon-Gwang <wkdgusrhkd@gmail.com>
2022-03-23 21:48:44 +0900

    FIX: production cors

--- app.js
+++ app.js
@@ -48,8 +48,8 @@ if (process.env.NODE_ENV === "production") {
   app.use(helmet({ contentSecurityPolicy: false }));
   app.use(
     cors({
-      origin: "*",
-      // credentials: true,
+      origin: "http://nemowithme.com",
+      credentials: true,
     })
   );
 } else {
