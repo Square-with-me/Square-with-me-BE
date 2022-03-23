e020e5175aab25dfae000810c841a7396a347ab3
Hyeon-Gwang <wkdgusrhkd@gmail.com>
2022-03-24 00:45:40 +0900

    FIX: cookie sameSite=none

--- controllers/authController.js
+++ controllers/authController.js
@@ -369,12 +369,12 @@ module.exports = {
       res.cookie("accessToken", accessToken, {
         httpOnly: true,
         path: "/",
-        sameSite: "lax",
+        sameSite: "none",
       }); //options 참고 : https://www.npmjs.com/package/cookie
       res.cookie("refreshToken", refreshToken, {
         httpOnly: true,
         path: "/",
-        sameSite: "lax",
+        sameSite: "none",
       });
 
       const firstComeBadge = await Badge.findOne({
