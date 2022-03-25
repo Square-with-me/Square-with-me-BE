62c912096042b2d9b1fc6e67dfbcc0cd05024fa2
Hoon333 <wkdzhd2000@gmail.com>
2022-03-26 02:40:12 +0900

    ADD: User new column seeder added

---
+++ seeders/initUser.js
@@ -0,0 +1,31 @@
+'use strict';
+
+module.exports = {
+  async up (queryInterface, Sequelize) {
+    /**
+     * Add seed commands here.
+     *
+     * Example:
+     * await queryInterface.bulkInsert('People', [{
+     *   name: 'John Doe',
+     *   isBetaMember: false
+     * }], {});
+    */
+  //  일회성 컬럼 추가용
+     queryInterface.addColumn('users', 'lastUpdated', {
+      type: DataTypes.DATE,
+        allowNull: false,
+      defaultValue: new Date()
+    })
+
+  },
+
+  async down (queryInterface, Sequelize) {
+    /**
+     * Add commands to revert seed here.
+     *
+     * Example:
+     * await queryInterface.bulkDelete('People', null, {});
+     */
+  }
+};
