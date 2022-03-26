d401f05b080d384eeaa9279a378bf07ebe6be44c
Hyeon-Gwang <wkdgusrhkd@gmail.com>
2022-03-26 20:54:41 +0900

    FIX: kakao login getMyBadges where

--- controllers/authController_notCookie.js
+++ controllers/authController_notCookie.js
@@ -162,9 +162,7 @@ module.exports = {
             },
           });
           const isGivenBadge = await user.getMyBadges({
-            where: {
-              where: { id: firstComeBadge.id, },
-            },
+            where: { id: firstComeBadge.id, },
           });
 
           const leftBadge = firstComeBadge.leftBadges;
