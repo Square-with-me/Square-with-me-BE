713b8e0477fdc6004b7cc8b8438b5937d8356d8c
Hyeon-Gwang <wkdgusrhkd@gmail.com>
2022-03-29 06:23:36 +0900

    FIX: mongoose config

--- mongoSchemas/index.js
+++ mongoSchemas/index.js
@@ -1,18 +1,20 @@
-const dotenv = require("dotenv");
-dotenv.config();
 const mongoose = require("mongoose");
-const { MONGO_DB_URL } = process.env;
-const krToday = require("../utils/date").koreanDate()
+const { koreanDate: krToday } = require("../utils/date");
 
 // Mongo DB 연결 부분
 const connect = () => {
   mongoose
-      .connect(MONGO_DB_URL, {
-          useNewUrlParser: true,
-          useUnifiedTopology: true,
-          ignoreUndefined: true,
-      }).then(() => console.log("MongoDB Connected", krToday))
-      .catch(err => console.log(err));
+    .connect(
+      process.env.NODE_ENV === "production"
+        ? process.env.NEMO_WITH_ME_DB_PRODUCTION_URL
+        : process.env.NEMO_WITH_ME_DB_URL
+      , {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+        ignoreUndefined: true,
+      })
+    .then(() => console.log("MongoDB Connected", krToday()))
+    .catch(err => console.log(err));
 };
 
 mongoose.connection.on("error", err => {
