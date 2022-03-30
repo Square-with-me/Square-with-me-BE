aaf13f9dac1a4d60de6001c2b2ac3c92d2065820
Hyeon-Gwang <wkdgusrhkd@gmail.com>
2022-03-30 13:57:59 +0900

    config development 설정을 production으로

--- config/config.js
+++ config/config.js
@@ -3,10 +3,10 @@ dotenv.config();
 
 module.exports = {
   "development": {
-    "username": "root",
-    "password": process.env.SQUARE_WITH_ME_DB_PASSWORD,
-    "database": "Square_with_me_database",
-    "host": "127.0.0.1",
+    "username": process.env.MYSQL_PRODUCTION_DB_USERNAME,
+    "password": process.env.MYSQL_PRODUCTION_DB_PASSWORD,
+    "database": "nemo_with_me_database",
+    "host": process.env.MYSQL_PRODUCTION_DB_HOST,
     "dialect": "mysql",
     "timezone": "+09:00"
   },
