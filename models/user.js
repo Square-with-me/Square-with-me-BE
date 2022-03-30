150d39073222a632db95d9365aef8244b5e35ba9
Hoon333 <wkdzhd2000@gmail.com>
2022-03-31 01:26:49 +0900

    FIX: first rebase15

--- models/user.js
+++ models/user.js
@@ -1,16 +1,4 @@
-
-// 1. 현재 PC 표준 시간
-const curr = new Date();
-
-// 2. UTC 시간 계산
-const utc = 
-      curr.getTime() + 
-      (curr.getTimezoneOffset() * 60 * 1000);
-
-// 3. UTC to KST (UTC + 9시간)
-const KR_TIME_DIFF = 9 * 60 * 60 * 1000;
-const kr_curr = 
-      new Date(utc + (KR_TIME_DIFF));
+const krToday = require("../utils/timeRecord").koreanDate()
 
 module.exports = (sequelize, DataTypes) => {
   const User = sequelize.define(
@@ -53,7 +41,7 @@ module.exports = (sequelize, DataTypes) => {
       lastUpdated: {
         type: DataTypes.DATE,
         allowNull: false,
-        default: kr_curr,
+        default: krToday,
       },
     },
     {
